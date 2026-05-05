4612ab89aef526bd77a7acc425fb5ae432b00aed
durumyisking <cww9512@gmail.com>
2026-05-05 13:54:03 +0900

    Feat : Cosmetic관련 컴포넌트 추가 및 바인드, TaggedActor클래스추가, RandomCharacter, SelectCharacter ControllerComponent추가 정삭작동 확

---
+++ Source/Overwatch/AbilitySystem/OWTaggedActor.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagAssetInterface.h"
+#include "GameFramework/Actor.h"
+#include "OWTaggedActor.generated.h"
+
+UCLASS()
+class OVERWATCH_API AOWTaggedActor : public AActor, public IGameplayTagAssetInterface
+{
+	GENERATED_BODY()
+
+public:
+
+	AOWTaggedActor(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	//~IGameplayTagAssetInterface
+	virtual void GetOwnedGameplayTags(FGameplayTagContainer& TagContainer) const override;
+	//~End of IGameplayTagAssetInterface
+
+	//~UObject interface
+#if WITH_EDITOR
+	virtual bool CanEditChange(const FProperty* InProperty) const override;
+#endif
+	//~End of UObject interface
+
+protected:
+	// Gameplay-related tags associated with this actor
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Actor)
+	FGameplayTagContainer StaticGameplayTags;
+};
+
